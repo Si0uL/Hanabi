cd741cfb9ed9fc960188fb504445782ad982d35c
Louis Mangin <mangin.louis@gmail.com>
2018-01-06 23:09:36 +0100

    Added chat + refresh

--- views/controllers/gameController.js
+++ views/controllers/gameController.js
@@ -5,7 +5,33 @@ angular
 function gameController( $scope, $state, userService ) {
 
     $scope.gameData = userService.getGame();
+    $scope.socket = userService.getSocket();
+    $scope.username = userService.getUser();
     console.log($scope.gameData);
     $scope.highlighted = new Array($scope.gameData.cardsPerPlayer).fill(false);
+
+    // Chat
+    $scope.toSend = { text:'' };
+    $scope.messages = [];
+
+    $scope.socket.emit('message_history_request');
+
+    $scope.socket.on('message_history', function(history) {
+        $scope.messages = history;
+        $scope.messages.forEach(function(elt,n) {
+            if (elt.pseudo === $scope.username) $scope.messages[n].pseudo = 'You'
+        });
+        if (!$scope.$$phase) $scope.$digest();
+    });
+
+    $scope.send = function() {
+        $scope.socket.emit('message', $scope.toSend.text);
+        $scope.toSend.text = '';
+    };
+
+    $scope.socket.on('message', function(data) {
+        $scope.messages = [{pseudo: data.pseudo, message: data.message}].concat($scope.messages);
+        if (!$scope.$$phase) $scope.$digest();
+    });
 
 }
